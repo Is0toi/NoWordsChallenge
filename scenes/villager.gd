5d9cb0b10030495816eec05fe30651d3dac45b82
David S <dshi2008mayo@gmail.com>
2025-08-09 18:21:28 -0400

    all villagers working

---
+++ scenes/villager.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+@onready var bubble: AnimatedSprite2D = $AnimatedSprite2D2
+var player_in_range := false
+
+func _ready():
+	$AnimatedSprite2D.play("default")
+	$InteractionZone.body_entered.connect(_on_body_entered)
+	$InteractionZone.body_exited.connect(_on_body_exited)
+	
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		player_in_range = true
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_in_range = false
+		
+func _process(delta):
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		bubble.visible = !bubble.visible
+	
